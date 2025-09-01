9d76ba9121f1a4d36d926c64393fcc8c12e5e056
JYB0535 <jnk8528@gmail.com>
2025-09-01 17:53:58 +0900

    2025-09-01
    2025-09-01

--- src/api/carApi.ts
+++ src/api/carApi.ts
@@ -1,35 +1,43 @@
+import axios from "axios";
 import type { Car } from "../type";
 
 //자동차 목록 조회
-export const getCars = (): Promise<Car[]> => { //Promise추가해줌 왜였지 들었는데 생각이 안나 
-    //서버로 데이터 조회 ==> 응답 데이터 리턴
-    const getCarsDummy = [ //이 함수에서 반환해야할게 배열이라서 대괄호로 시작
-        {   id: 1,
-            brand: 'Ford',
-            model: 'Mustang',
-            color: 'Red',
-            registrationNumber: "ADF-1121",
-            modelYear: 2023,
-            price: 59000
-        },
-        {   id: 2,
-            brand: 'bmw',
-            model: 'poly',
-            color: 'white',
-            registrationNumber: "BCD-1234",
-            modelYear: 2024,
-            price: 69000
-        },
-        {   id: 3,
-            brand: 'kia',
-            model: 'water',
-            color: 'blue',
-            registrationNumber: "CDF-2345",
-            modelYear: 2025,
-            price: 79000
-        },
-    ]
-    return Promise.resolve(getCarsDummy); //이러면 getCarsDummy를 감싸는 Promise가 반환된다. then과 catch 사용가능 
+export const getCars = async (): Promise<Car[]> => { //Promise추가해줌 왜였지 들었는데 생각이 안나 
+    
+    const response = await axios.get("http://localhost:8080/cars");
+    return response.data;
+
+
+
+        //이제 더미 데이터 필요없음
+        //서버로 데이터 조회 ==> 응답 데이터 리턴
+    // const getCarsDummy = [ //이 함수에서 반환해야할게 배열이라서 대괄호로 시작
+    //     {   id: 1,
+    //         brand: 'Ford',
+    //         model: 'Mustang',
+    //         color: 'Red',
+    //         registrationNumber: "ADF-1121",
+    //         modelYear: 2023,
+    //         price: 59000
+    //     },
+    //     {   id: 2,
+    //         brand: 'bmw',
+    //         model: 'poly',
+    //         color: 'white',
+    //         registrationNumber: "BCD-1234",
+    //         modelYear: 2024,
+    //         price: 69000
+    //     },
+    //     {   id: 3,
+    //         brand: 'kia',
+    //         model: 'water',
+    //         color: 'blue',
+    //         registrationNumber: "CDF-2345",
+    //         modelYear: 2025,
+    //         price: 79000
+    //     },
+    //]
+    //return Promise.resolve(getCarsDummy); //이러면 getCarsDummy를 감싸는 Promise가 반환된다. then과 catch 사용가능 
 }
 
 export const deleteCar = (id: number): Promise<number> => {
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  //<StrictMode> 얘 때문에 마운트나 그런ㄴ게 2번씩 되서 제거 
     <App />
-  </StrictMode>,
+  //</StrictMode>,
 )
